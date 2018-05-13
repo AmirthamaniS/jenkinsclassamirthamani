53dfa8da3decede91ed8fdb5096e280923df4841
Manivannan <Manivannan@DESKTOP-FOBC3U9>
2018-05-13 11:20:05 +0530

    new pro

---
+++ newprojectforjenkinsgit/src/newprojectforjenkinsgit/Newclass.java
@@ -0,0 +1,12 @@
+package newprojectforjenkinsgit;
+
+public class Newclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("selenium");
+
+	}
+
+}
